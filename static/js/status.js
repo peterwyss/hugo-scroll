b93f48a410b8041a5bce5f4bb4353d986b996465
Peter Wyss <peter-wyss@bluewin.ch>
2022-06-03 21:00:16 +0200

    Schliesst bald

--- static/js/status.js
+++ static/js/status.js
@@ -8,13 +8,23 @@ $.getJSON( "/js/status.json", function( data ) {
     console.log(status)
     var elem1 = document.getElementById("offen");
     var elem2 = document.getElementById("geschlossen");
+    var elem3 = document.getElementById("bald");
 
     if( status == "geschlossen") {
         elem1.style.visibility = "hidden"
         elem2.style.visibility = "visible"
-    }else {
+        elem3.style.visibility = "hidden"
+
+    }else if( status == "offen"){
         elem1.style.visibility = "visible";
         elem2.style.visibility = "hidden";
+        elem3.style.visibility = "hidden"
+
+    }else{
+        elem1.style.visibility = "hidden";
+        elem2.style.visibility = "hidden";
+        elem3.style.visibility = "visible"
+        console.log("Test");
     }
     
 });
