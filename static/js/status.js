8658918a9ff0cb1d515334ea917b3a2f43a917f4
Peter Wyss <peter-wyss@bluewin.ch>
2022-07-24 09:41:12 +0200

    status

--- static/js/status.js
+++ static/js/status.js
@@ -1,29 +1,38 @@
 var elem_o = "<div class='status-offen' >Offen</div>";
+var elem_b = "<div class='status-bald' >Schliesst bald</div>";
 var elem_g = "<div class='status-geschlossen' >Geschlossen</div>";
+
+var elem_g = document.createElement('div');
+elem_g.innerHTML = "Geschlossen";
+elem_g.className = "status-geschlossen";
+
+var elem_b = document.createElement('div');
+elem_b.innerHTML = "Schliesst bald";
+elem_b.className = "status-bald-geschlossen";
+
+var elem_o = document.createElement('div');
+elem_o.innerHTML = "Offen";
+elem_o.className = "status-offen";
+
+
 var status = "";
 
 $.getJSON( "/js/status.json", function( data ) {
     console.log( data );
     status = data.badeplatz.status;
-    console.log(status)
-    var elem1 = document.getElementById("offen");
-    var elem2 = document.getElementById("geschlossen");
-    var elem3 = document.getElementById("bald");
-
+    console.log("Status: ",status)
+    //var elem_status = document.getElementById("status");
+   
     if( status == "geschlossen") {
-        elem1.style.visibility = "hidden"
-        elem2.style.visibility = "visible"
-        elem3.style.visibility = "hidden"
+      console.log("status geschlossen ")  
+      document.getElementById('status').appendChild(elem_g);
 
     }else if( status == "offen"){
-        elem1.style.visibility = "visible";
-        elem2.style.visibility = "hidden";
-        elem3.style.visibility = "hidden"
+        document.getElementById('status').appendChild(elem_o);
 
     }else{
-        elem1.style.visibility = "hidden";
-        elem2.style.visibility = "hidden";
-        elem3.style.visibility = "visible"
+        document.getElementById('status').appendChild(elem_b);
+
     }
     
 });
