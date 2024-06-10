478b72a9f18bd47c4b3f9f15526c5680644e1642
Peter Wyss <peter-wyss@bluewin.ch>
2024-06-10 23:22:38 +0200

    polling

--- static/js/status.js
+++ static/js/status.js
@@ -1,40 +1,57 @@
-var elem_o = "<div class='status-offen' >Offen</div>";
-var elem_b = "<div class='status-bald' >Schliesst bald</div>";
-var elem_g = "<div class='status-geschlossen' >Geschlossen</div>";
-
+// Elemente für verschiedene Status erstellen
 var elem_g = document.createElement('div');
-elem_g.innerHTML = "Geschlossen";
-elem_g.className = "status-geschlossen";
+elem_g.innerHTML = 'Geschlossen';
+elem_g.className = 'status-geschlossen';
 
 var elem_b = document.createElement('div');
-elem_b.innerHTML = "Offen bis 17.09";
-elem_b.className = "status-bald-geschlossen";
+elem_b.innerHTML = 'Offen bis 17:00';
+elem_b.className = 'status-bald-geschlossen';
 
 var elem_o = document.createElement('div');
-elem_o.innerHTML = "Offen ";
-elem_o.className = "status-offen";
-
+elem_o.innerHTML = 'Offen';
+elem_o.className = 'status-offen';
 
 var status = "";
-$(document).ready(function() {
-    $.ajaxSetup({ cache: false });
-  });
-$.getJSON( "/js/status.json", function( data ) {
-    console.log( data );
-    status = data.badeplatz.status;
-    console.log("Status: ",status)
-    //var elem_status = document.getElementById("status");
-   
-    if( status == "geschlossen") {
-      console.log("status geschlossen ")  
-      document.getElementById('status').appendChild(elem_g);
 
-    }else if( status == "offen"){
-        document.getElementById('status').appendChild(elem_o);
+// Funktion zum Abrufen und Aktualisieren des Status
+async function fetchAndUpdateStatus() {
+try {
+const response = await fetch('/js/status.json');
+if (!response.ok) {
+throw new Error('Netzwerkantwort war nicht in Ordnung');
+}
+const data = await response.json();
+status = data.badeplatz.status;
+console.log("Status:", status);
+updateStatus(status);
+} catch (error) {
+console.error('Es gab ein Problem mit der Fetch-Operation:', error);
+}
+}
 
-    }else{
-        document.getElementById('status').appendChild(elem_b);
+// Funktion zur Aktualisierung der Benutzeroberfläche basierend auf dem Status
+function updateStatus(status) {
+const statusElement = document.getElementById('status');
+statusElement.innerHTML = ''; // Vorherigen Inhalt leeren
 
-    }
-    
-});
+
+if (status === "geschlossen") {
+    console.log("Status geschlossen");
+    statusElement.appendChild(elem_g);
+} else if (status === "offen") {
+    statusElement.appendChild(elem_o);
+} else {
+    statusElement.appendChild(elem_b);
+}
+
+
+}
+
+// Polling alle 5 Sekunden
+function startPolling(interval) {
+fetchAndUpdateStatus(); // Erste Abruf
+setInterval(fetchAndUpdateStatus, interval); // Regelmäßiger Abruf
+}
+
+// Polling starten
+startPolling(5000); // Aktualisierung alle 5 Sekunden
